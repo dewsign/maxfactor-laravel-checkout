acb3744bea970ff14ed964d54def3d3b6fd454f8
Tristan Ward <tristan.ward@dewsign.co.uk>
2018-01-23 14:52:33 +0000

    route middleware update

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web'], 'prefix' => 'checkout', 'as' => 'checkout.'], function () {
+Route::group(['middleware' => 'web', 'prefix' => 'checkout', 'as' => 'checkout.'], function () {
     Route::get('/', 'Maxfactor\Checkout\CheckoutController@index')->name('index');
     Route::get('{uid}/{stage?}', 'Maxfactor\Checkout\CheckoutController@show')->name('show');
     Route::post('{uid}/{stage?}', 'Maxfactor\Checkout\CheckoutController@store')->name('store');
