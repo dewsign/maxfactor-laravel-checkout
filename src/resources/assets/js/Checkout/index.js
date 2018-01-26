0ae6117c2179243696b8d54a945dfb996cf13bc4
Marco Mark <marco@silvanite.com>
2018-01-26 16:20:34 +0000

    Basic working default cart page with discount component

--- src/resources/assets/js/Checkout/index.js
+++ src/resources/assets/js/Checkout/index.js
@@ -1,7 +1,9 @@
 import MxCartItem from './Components/CartItem.vue'
+import MxCartDiscount from './Components/CartDiscount.vue'
 
 export default {
     install(Vue) {
-        Vue.component(MxCartItem)
+        Vue.component(MxCartItem),
+        Vue.component('mx-cart-discount', MxCartDiscount)
     },
 }
