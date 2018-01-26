acea234ca0ed877b5ac4ce9f3dec787da16c028e
Marco Mark <marco@silvanite.com>
2018-01-26 10:38:34 +0000

    Include Views, translations and vue components required for the cart and load them in the service provider

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -13,11 +13,11 @@ class CheckoutServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        $this->publishes([
-            __DIR__.'/config/maxfactor-checkout.php' => config_path('maxfactor-checkout.php')
-        ]);
+        $this->bootRoutes();
+        $this->bootConfig();
+        $this->bootTranslations();
+        $this->bootViews();
+        $this->bootAssets();
     }
 
     /**
@@ -27,8 +27,54 @@ class CheckoutServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config/maxfactor-checkout.php', 'maxfactor-checkout');
+        $this->registerConfig();
+        $this->registerBindings();
+    }
 
+    private function bootRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+
+    private function bootConfig()
+    {
+        $this->publishes([
+            __DIR__.'/config/maxfactor-checkout.php' => config_path('maxfactor-checkout.php')
+        ]);
+    }
+
+    private function bootTranslations()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'maxfactor');
+
+        $this->publishes([
+            __DIR__.'/resources/lang' => resource_path('lang/vendor/maxfactor'),
+        ]);
+    }
+
+    private function bootViews()
+    {
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'maxfactor');
+
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/maxfactor'),
+        ]);
+    }
+
+    private function bootAssets()
+    {
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js/vendor/maxfactor'),
+        ], 'js');
+    }
+
+    private function registerConfig()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/maxfactor-checkout.php', 'maxfactor-checkout');
+    }
+
+    private function registerBindings()
+    {
         $this->app->bind('Maxfactor\Checkout\Contracts\Checkout', config('maxfactor-checkout.driver'));
     }
 }
---
+++ src/resources/assets/js/Checkout/index.js
@@ -0,0 +1,7 @@
+import MxCartItem from './Components/CartItem.vue'
+
+export default {
+    install(Vue) {
+        Vue.component(MxCartItem)
+    },
+}
