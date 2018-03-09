384d072d32e8a9783c4556400c0e4aada0e86aa6
Tristan Ward <tristan.ward@dewsign.co.uk>
2018-03-09 15:50:34 +0000

    put config back as this is fft specific

--- src/config/maxfactor-checkout.php
+++ src/config/maxfactor-checkout.php
@@ -3,5 +3,5 @@
 return [
     "driver" => "App\Checkout",
     "postage" => "App\Postage",
-    "minimum_order" => env('MINIMUM_ORDER_VALUE', 35.00),
+    "minimum_order" => env('MINIMUM_ORDER_VALUE', 1.00),
 ];
