792ec2204a2dc5f41e1a8d657ce8c249de0691e0
Marco Mark <marco@silvanite.com>
2018-05-31 15:43:31 +0100

    Load the new postcode lookup component by default

--- src/resources/assets/js/Checkout/index.js
+++ src/resources/assets/js/Checkout/index.js
@@ -1,11 +1,13 @@
 import MxCartItem from './Components/CartItem.vue'
 import MxCartDiscount from './Components/CartDiscount.vue'
 import MxStripe from './Components/Stripe.vue'
+import MxAddress from './Components/AddressLookup.vue'
 
 export default {
     install(Vue) {
-        Vue.component('mx-cart-item', MxCartItem),
+        Vue.component('mx-cart-item', MxCartItem)
         Vue.component('mx-cart-discount', MxCartDiscount)
         Vue.component('mx-stripe', MxStripe)
+        Vue.component('mx-address', MxAddress)
     },
 }
