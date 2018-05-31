c6757f7be7f93477c3f969501035fef6a4518ee7
Marco Mark <marco@silvanite.com>
2018-05-31 15:42:49 +0100

    Allow the PCA Predict api key to be defined via environment variables

--- src/config/maxfactor-checkout.php
+++ src/config/maxfactor-checkout.php
@@ -3,6 +3,7 @@
 return [
     "driver" => "App\Checkout",
     "postage" => "App\Postage",
+    "pca_key" => env('PCA_KEY'),
     "minimum_order" => env('MINIMUM_ORDER_VALUE', 1.00),
     "checkout_stages" => [
         'DEFAULT' => '1',
