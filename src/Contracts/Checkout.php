d816be0b1e7e274c80ab6e58015fc87b24775e27
Marco Mark <marco@silvanite.com>
2018-08-13 15:42:22 +0100

    Avoid duplicate payments

--- src/Contracts/Checkout.php
+++ src/Contracts/Checkout.php
@@ -8,4 +8,11 @@ interface Checkout
      * Within the construct, the `content` attribute must be populated with the data from the
      * checkout store / handler. (e.g. remote api or local class)
      */
+
+    /**
+     * Must return true or false depending on whether payment should be taken
+     *
+     * @return boolean
+     */
+    public function isPaymentRequired();
 }
