cbec564fbd37734aa46f951d4c64d84b1cd5e696
Tristan Ward <tristan.ward@dewsign.co.uk>
2018-09-20 16:22:35 +0100

    discount error block

--- src/resources/views/checkout/components/summary.blade.php
+++ src/resources/views/checkout/components/summary.blade.php
@@ -1,4 +1,8 @@
 <div class="checkout__summary">    
+    <div class="checkout__discount-error" v-if="currentCheckout.discount.error">
+        @lang('Discount invalid:')
+        <span>@{{ currentCheckout.discount.error }}</span>
+    </div>
     <div class="checkout__subtotal">
         @lang('Net total:')<span>@{{ cartNetTotal | money }}</span>
     </div>
@@ -6,10 +10,6 @@
         @lang('Discount applied:')
         <template v-if="currentCheckout.discount.percentage && currentCheckout.discount.percentage > 0">@{{ currentCheckout.discount.percentage | percentage }}</template>
         <span>@{{ cartDiscountTotal | money }}</span>
-    </div>
-    <div class="checkout__subtotal error" v-if="currentCheckout.discount.error">
-        @lang('Discount:')
-        <span>@{{ currentCheckout.discount.error }}</span>
     </div>
     {{-- TODO: Display only after step 1 has been completed   --}}
     <div class="checkout__shipping">
