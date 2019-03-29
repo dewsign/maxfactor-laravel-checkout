9522dc2429cff45d0722dfccfa6f0fd778b0188e
tristanward <tristan.ward@dewsign.co.uk>
2019-03-29 11:22:07 +0000

    do not check on first stage of checkout

--- src/Traits/HandlesCheckout.php
+++ src/Traits/HandlesCheckout.php
@@ -61,7 +61,14 @@ trait HandlesCheckout
      */
     public function stage(string $stage = null, string $mode = 'show')
     {
-        if ($mode === 'show' && $this->getFirst('finalTotal') < config('maxfactor-checkout.minimum_order')) {
+        /**
+         * Ensure a customer cannot progress through the checkout if their order drops below the min value
+         * This could happen if a product becomes unavailable and is removed from the cart
+         * This is not being checked on the first stage as the finalValue will not have been updated here
+         */
+        if ($stage !== 'default'
+            && $mode === 'show'
+            && $this->getFirst('finalTotal') < config('maxfactor-checkout.minimum_order')) {
             Session::put('checkoutError', 'Order value error');
             header('Location: ' . route('cart.index'));
         }
